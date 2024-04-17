ed5787534cb382f01d9de7ba708d70f60327aa94
aditeleanjar <adi.teleanjar@gmail.com>
2024-04-17 21:07:20 +0700

    update url

--- lib/config.php
+++ lib/config.php
@@ -2,8 +2,11 @@
 
 //=====================CONFIG URL==============//
 
-define('BASE_URL', "http://localhost/backup/donasi/");
-define('BASE_URL_ADMIN', 'http://localhost/backup/donasi/admin');
+// define('BASE_URL', "http://localhost/backup/donasi/");
+// define('BASE_URL_ADMIN', 'http://localhost/backup/donasi/admin');
+
+define('BASE_URL', "http://139.99.102.235/donasiapp/");
+define('BASE_URL_ADMIN', 'http://139.99.102.235/donasiapp/admin');
 
 //=====================END CONFIG URL==============//
 
